cfd007b6b22a43f84f75a6426e293c7f6b49ecf5
kento.furusho <swiri761043@gmail.com>
2021-09-17 02:27:20 +0000

    signup

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
+  before_save { self.email.downcase! }
+  has_secure_password
+  
+  validates :name, presence:true, length: { maximum: 30 }
+  validates :email, presence:true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: { case_sensitive: false }
+end
