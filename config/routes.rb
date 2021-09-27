506b113accd0beb1a065c938c60262cb9058d225
kento.furusho <swiri761043@gmail.com>
2021-09-27 07:05:39 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,17 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new,:create, :edit, :destroy]
-
+  resources :users, only: [:index, :show, :new,:create, :edit, :destroy] do
+    member do 
+      get :followings
+      get :followers
+    end
+  end
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   resources :phrases, only: [:create, :destroy, :edit]
+  resources :relationships, only: [:create, :destroy]
 end
